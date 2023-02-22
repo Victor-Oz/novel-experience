dc14800c53993df829910956e8e82d86b71e6939
Victor Oz <victorozurumba12@gmail.com>
2023-02-22 21:57:44 +0100

    began testing som framer motion seqences

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,19 @@
 
 import { Link, useMatch, useResolvedPath, NavLink } from "react-router-dom";
 import MenuButton from "./MenuButton.jsx";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   return (
     <nav className='nav'>
-      <NavLink to='/Home' activeClassname='active' className='site-title'>
+      <motion.NavLink
+        animate={{ x: 100 , scale :1.2}}
+        transition={{ delay: 1 }}
+        to='/Home'
+        activeClassname='active'
+        className='site-title'>
         OZ
-      </NavLink>
+      </motion.NavLink>
       <ul>
         <MenuButton className='menu' width='92' height='92' />
       </ul>
