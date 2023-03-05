59dafe332e42b2606ec2b9698811ea458efe8fb9
Victor Oz <victorozurumba12@gmail.com>
2023-03-05 03:56:20 +0100

    whatsap layout added

--- src/components/DashContainer.jsx
+++ src/components/DashContainer.jsx
@@ -4,28 +4,29 @@ import { motion } from "framer-motion";
 function DashContainer() {
   return (
     <div>
-    <div className='dashboard-container'>
-      <motion.img
-        className='space-man'
-        src='src\assets\flyboy.png'
-        initial={{ x: -300 }}
-        animate={{ x: -80 }}
-        transition={{ delay: 0.5 }}>
-     
-      </motion.img>
-      <div className='dashboard-text'>
-        Discover the hidden gems of YOUR CITY
+      <div className='dashboard-container'>
+        <motion.img
+          className='space-man'
+          src='src\assets\flyboy.png'
+          initial={{ x: -300 }}
+          animate={{ x: -80 }}
+          transition={{ delay: 0.5 }}></motion.img>
+        <div className='dashboard-text'>
+          Discover the hidden gems of YOUR CITY
+        </div>
+        <motion.img
+          className='dash-alien'
+          src='src\assets\alien.png'
+          initial={{ scale: 1.4 }}
+          animate={{ x: -40 ,scale: 1}}
+          transition={{ delay: 0.7 }}></motion.img>
       </div>
-      <motion.img
-        className='dash-alien'
-        src='src\assets\alien.png'
-        initial={{ x: 300 }}
-        animate={{ x: -40 }}
-        transition={{ delay: 0.7 }}>
-     
-      </motion.img>
-      </div>
-      <button className="dash-button" >Get Started</button>
+      <motion.button
+        whileHover={{ scale: 1.1  }}
+        whileTap={{ scale: 0.9  }}
+        className='dash-button'>
+        Get Started
+      </motion.button>
     </div>
   );
 }
--- src/components/StaticBlob.jsx
+++ src/components/StaticBlob.jsx
@@ -1,20 +1,41 @@
-import Park from  '../assets/park.jpg';
+/** @format */
+import { motion, useScroll } from "framer-motion";
 
-function StaticBlob(){
-    return(
-<div className='static-blob-container'>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        <div className="item pleasure-park"><img src={Park}></img></div>
-        </div>
-    )
+function StaticBlob() {
+  return (
+    <div className='chat-box-container'>
+      <motion.div
+        initial={{ x: -40, opacity: 0.5, scale: 0.9 }}
+        whileInView={{ x: 0, opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.5 }}
+        className='left-chat-container'>
+        <div className='left-chat'>Hi 👋</div>
+        <div className='left-chat'>You introverted Legend</div>
+        <div className='left-chat'>Are you trying to go out more</div>
+      </motion.div>
+      <motion.div
+        initial={{ x: 70, opacity: 0.5, scale: 0.9 }}
+        whileInView={{ x: 0, opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.7 }}
+        className='right-chat-container'>
+        <img className='chat-gif' src='src\assets\giphy.gif'></img>
+        <div className='right-chat '>Maybe some other time </div>
+      </motion.div>
+      <motion.div
+        initial={{ x: -40, opacity: 0.5, scale: 0.9 }}
+        whileInView={{ x: 0, opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.7 }}
+        className='left-chat-container'>
+        <div className='left-chat'>No i'snt an option 😂😂</div>
+        <div className='left-chat'>I'll change your mind</div>
+        {/* <img src="src\assets\pleasure.webp" alt="pleasure park" className="chat-image" />
+      <img src="src\assets\boomtown.jpg" alt="pleasure park" className="chat-image" /> */}
+      </motion.div>
+    </div>
+  );
 }
 
-export default StaticBlob
+export default StaticBlob;
