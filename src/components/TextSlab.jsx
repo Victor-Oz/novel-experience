98ff11a4031fdb5afd78a35b3490eacde362eba2
Victor Oz <victorozurumba12@gmail.com>
2023-03-10 01:06:51 +0100

    text slab scalable

--- src/components/TextSlab.jsx
+++ src/components/TextSlab.jsx
@@ -5,22 +5,19 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 function TextSlab() {
   const { scrollYProgress } = useScroll();
-  const x = useTransform(scrollYProgress, [0, 1], [400, -5100]);
-  const y = useTransform(scrollYProgress, [0, 1], [400, -5100]);
-  const z = useTransform(scrollYProgress, [0, 1], [400, -5100]);
-  return (
-    <div className='slab-container'>
+
+    const x = useTransform(scrollYProgress, [0, 1], [900, -9300]);
+    return (
+      <div className='slab-container'>
       <motion.div className='text-slab1 ' style={{ x }}>
-        explore our city
+        Who's World is this? , the world is Yours
       </motion.div>
-      <motion.div className=' text-slab2  ' style={{ y }}>
-        explore our city
-      </motion.div>
-      <motion.div className='text-slab3 ' style={{ z }}>
-        explore our city
-      </motion.div>
+     
     </div>
-  );
+    )
+
+ 
+  
 }
 
 export default TextSlab;
