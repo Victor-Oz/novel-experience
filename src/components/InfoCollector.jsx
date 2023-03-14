2782eca425e177e820fefe6198d0f010ad8fbb05
Victor Oz <victorozurumba12@gmail.com>
2023-03-14 23:44:03 +0100

    added dynamic navbar using use state

--- src/components/InfoCollector.jsx
+++ src/components/InfoCollector.jsx
@@ -1,18 +1,23 @@
+/** @format */
+
 function InfoCollector() {
-    
   return (
-    <div className="input-container">
-      <div className="top-input-container">
-      <div className='h3 red'>Craving new experiencees but</div>
-      <div className='h3 purple'>Don't know where to start ?</div>
-      <p>You just missed the summer edition.</p>
-      <p className="h3">We’ll keep you posted.</p>
+    <div className='input-container'>
+      <div className='top-input-container'>
+        <div className='h3 red'>Craving new experiencees but</div>
+        <div className='h3 purple'>Don't know where to start ?</div>
+        <p>You just missed the summer edition.</p>
+        <p className='h3'>We’ll keep you posted.</p>
       </div>
-      <div className="input-field-container">
-     <input className="first-name" type="text"  placeholder="   First Name"  /> 
-     <input className="last-name" type="text" placeholder="   Email Address" />
-     </div>
-     <button className="submit-btn">SUBSCRIBE  > </button>
+      <form className='input-field-container'>
+        <input className='first-name' type='text' placeholder='   First Name' />
+        <input
+          className='last-name'
+          type='text'
+          placeholder='   Email Address'
+        />
+      </form>
+      <button className='submit-btn'>SUBSCRIBE </button>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,33 @@
 /** @format */
 
 import { Link, useMatch, useResolvedPath, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { menu } from "react-icons-kit/feather/menu";
+import { x } from "react-icons-kit/feather/x";
+
 import MenuButton from "./MenuButton.jsx";
 import { motion } from "framer-motion";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
   return (
-    <nav className='nav'>
+    <nav className={toggle ? "navbar expanded" : "navbar"}>
       <NavLink to='/Home' activeClassname='active' id='site-title'>
         <img     src='/logo.png'
           alt='site logo ' id='site-title' />
       </NavLink>
-      <ul className='menu'>
-        <MenuButton className='menu' width='42' height='42' />
+      <div className='toggle-icon' onClick={handleToggle}>
+        {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
+      </div>
+      <ul className='links'>
+      <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
       </ul>
     </nav>
   );
