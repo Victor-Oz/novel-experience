a4baf0eae9deb6b75c30ec8b37a135b994847274
Victor Oz <victorozurumba12@gmail.com>
2023-03-18 22:44:31 +0100

    corrected home merge errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
   };
   return (
     <nav className={toggle ? "navbar expanded" : "navbar"}>
-      <NavLink to='/' activeclassname='active' id='site-title'>
+      <NavLink to='/' id='site-title'>
         <img     src='/logo.png'
           alt='site logo ' id='site-title' />
       </NavLink>
