5300950d94fe651a33e84f07e28956df429b644b
Victor Ozurumba <victorozurumba12@gmail.com>
2023-04-19 23:16:00 +0100

    improved image speed and reliability

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -19,6 +19,10 @@ const Account = () => {
       <img className="account-profile-photo" src={user?.photoURL} alt="" />
 
       <div className="account-maintext">This weeks location is Asian Town </div>
+
+      <div className="account-subtext">
+        38 Forces Ave, Old GRA 500101, Port Harcourt, Rivers
+      </div>
       <div className="week-location-img-container">
         <img className="week-location-img" src="/asia1.jpg" alt="" />
         <img className="week-location-img" src="/asia2.jpg" alt="" />
@@ -26,7 +30,13 @@ const Account = () => {
         <img className="week-location-img" src="/asia4.jpg" alt="" />
         <img className="week-location-img" src="/asia5.jpg" alt="" />
         <img className="week-location-img" src="/asia6.jpg" alt="" />
-        <video className="week-location-img" width="320" height="320" controls autoplay muted>
+        <video
+          className="week-location-img"
+          width="320"
+          height="320"
+          controls
+          muted
+        >
           <source src="/asiavid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
